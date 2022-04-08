ae02e2031b5d44f7d415e57824cb232e0a076593
Jottero <lookslikecat@gmail.com>
2022-04-08 17:53:33 +0300

    status2

--- Assets/Scripts/Stats.cs
+++ Assets/Scripts/Stats.cs
@@ -4,8 +4,60 @@ using UnityEngine;
 
 public class Stats : MonoBehaviour
 {
+    public string name;
+    public string faction;
+    public string race;
+
     public int health;
     public int damage;
-    public int speed;
+    public float maxspeed;
+    public float speed;
     public int range;
+    public List<Status> statuslist;
+    public List<string> enemylist;
+    
 }
+
+public class Status
+{
+    string name;
+    float duration;
+    string strength;
+    public Status(string n, float dur, string str)
+    {
+        name = n;
+        strength = str;
+        duration = dur;
+    }
+
+    public bool applyEffect(Status status, Stats stats)
+    {
+
+        if (status.duration - Time.deltaTime > 0)
+        {
+
+            if (status.name == "stun")
+            {
+                status.duration -= Time.deltaTime;
+                stats.speed = 0;
+            }
+
+            return true;
+        }
+        else
+        {
+            //duration = 0;
+            //a.speed = baseSpeed;
+
+            //return false;
+        }
+
+
+        return true;
+    }
+    public void applyEffect(int strength, string Statustype, GameObject target)
+    {
+
+    }
+}
+
