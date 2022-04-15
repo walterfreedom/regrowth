e775cc5a0f85cccd0458a5a707d0f933c767d99f
Jottero <lookslikecat@gmail.com>
2022-04-15 13:09:49 +0300

    aaa

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -9,13 +9,17 @@ public class movement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public GameObject attackpoint;
+    bool isranged = true;
+    public GameObject gun;
 
 
     // Start is called before the first frame update
 
-    void start()
+    private void Start()
     {
-        transform.Find("attackpoint");
+        
+        attackpoint = gameObject.transform.Find("atk").gameObject;
+        gun = attackpoint.transform.Find("gun").gameObject;
         rb = gameObject.GetComponent<Rigidbody2D>();
         
          
@@ -44,8 +48,26 @@ public class movement : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(inputX * 6, inputY * 6);
 
-        if (inputX != 0 || inputY != 0)
+        if (inputX != 0 && !isranged || inputY != 0 && !isranged)
         attackpoint.transform.position = new Vector2(gameObject.transform.position.x + Sign(inputX), gameObject.transform.position.y + Sign(inputY));
+        if (isranged)
+        {
+            
+            Vector3 mousePosition = transform.Find("Main Camera").GetComponent<Camera>().ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            Vector3 shootdirection = (mousePosition - gameObject.transform.position).normalized;
+            if (transform.position.x - mousePosition.x > 0)
+            {
+                gun.GetComponent<SpriteRenderer>().flipY=true;
+            }
+            else{
+                gun.GetComponent<SpriteRenderer>().flipY = false;
+            }
+            float angle = Mathf.Atan2(shootdirection.x, shootdirection.y) * Mathf.Rad2Deg;
+            attackpoint.transform.eulerAngles = new Vector3(0, 0, 90-angle);
+
+        }
+
         //rb.AddForce(Player.transform.TransformVector(new Vector2(inputX, inputY)) * 2.0f); //snoww!!!
     }
     
